6853da01d82744e71257074e01f7285d73013285
DreadnaughSec <brianrpeters@gmail.com>
2017-07-18 19:05:00 -0500

    small update to script with some info

--- texter.py
+++ texter.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 import smtplib
-server = smtplib.SMTP('smtp.sendgrid.net', 587)
-server.login("fakebadguy", "123qweqwe")
-msg = "YOU just use 'OP 1' AND IT FIXES EVERYTHING ..."
+server = smtplib.SMTP('YOURSMTPSERVERIP', YOURSMPTSERVERPORT)
+server.login("USERNAME", "PASSWORD")
+msg = "PUT YOUR MESSAGE HERE"
 loopit = True
 i = 0
 while loopit:
-    server.sendmail("123-123-1234", "6179557130@txt.att.net", msg)
+    server.sendmail("SOURCE-NUMBER-HERE", "TARGETEMAILHERE", msg)
     i+=1
     if i == 2: #number of times to loop
         loopit = False
